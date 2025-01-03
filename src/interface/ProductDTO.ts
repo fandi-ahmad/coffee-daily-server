c0818b64245bdf79d4817951743128b32d02af06
fandi-ahmad <fandi4160@gmail.com>
2025-01-03 22:48:09 +0800

    add interface type for product, size and topping

---
+++ src/interface/ProductDTO.ts
@@ -0,0 +1,11 @@
+export interface ProductDTO {
+  id?: number
+  id_user_seller: number
+  photo: string
+  name: string
+  description: string
+  price: number
+  id_category: number
+  total_sales: number
+  total_earning: number
+}
---
+++ src/interface/ToppingSizeDTO.ts
@@ -0,0 +1,6 @@
+export interface ToppingSizeDTO {
+  id?: number
+  name: string
+  price: number
+  id_user: number
+}
